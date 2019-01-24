d93cb591022c3a3e842c6a890911ee198b139ce9
Junya Ogasawara <ogasawara.junya@moneyforward.co.jp>
2019-01-24 13:48:43 +0900

    Catch all undefined pathes

--- main.go
+++ main.go
@@ -15,18 +15,43 @@ func main() {
 	appName := getAppName()
 	log.Printf("Running stub container for %s service", appName)
 
-	healthCheckPath := getHealthCheckPath()
-
 	r := mux.NewRouter()
-	r.HandleFunc("/", rootHandler)
-	r.HandleFunc(healthCheckPath, healthCheckHandler)
-	r.HandleFunc("/nettest/{host}/{port:[0-9]+}", netTestHandler)
+	r.HandleFunc("/nettest/{host}/{port:[0-9]+}", newNetTestHandler())
+	r.PathPrefix("/").HandlerFunc(newCatchAllHandler())
 
 	addr := getListenAddr()
 	if err := http.ListenAndServe(addr, r); err != nil {
 		log.Fatalf("unexpected error : %v", err)
 	}
 }
+func newCatchAllHandler() http.HandlerFunc {
+	appName := getAppName()
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "stub container for %s service\n", appName)
+	}
+}
+
+func newNetTestHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		target := vars["host"] + ":" + vars["port"]
+
+		timeout := time.Duration(3) * time.Second
+		_, err := net.DialTimeout("tcp", target, timeout)
+
+		if err != nil {
+			w.WriteHeader(http.StatusGatewayTimeout)
+			fmt.Fprintf(w, "Failed to establish tcp connection to %s\n", target)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Successfully established tcp connection to %s\n", target)
+
+	}
+}
+
 func getListenAddr() string {
 	port, found := os.LookupEnv("LISTEN_PORT")
 	if !found {
@@ -41,38 +66,4 @@ func getAppName() string {
 		return "undefined"
 	}
 	return name
-}
-
-func getHealthCheckPath() string {
-	path, found := os.LookupEnv("HEALTH_CHECK_PATH")
-	if !found {
-		return "/health_check"
-	}
-	return path
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	appName := getAppName()
-	fmt.Fprintf(w, "stub container for %s service\n", appName)
-}
-
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "ok\n")
-}
-
-func netTestHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	target := vars["host"] + ":" + vars["port"]
-
-	timeout := time.Duration(3) * time.Second
-	_, err := net.DialTimeout("tcp", target, timeout)
-
-	if err != nil {
-		w.WriteHeader(http.StatusGatewayTimeout)
-		fmt.Fprintf(w, "Failed to establish tcp connection to %s\n", target)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Successfully established tcp connection to %s\n", target)
 }
